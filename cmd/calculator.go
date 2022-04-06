bf4a1ea1a596b1f09621368526e943d2803a93aa
aemakeye <aemakeye@gmail.com>
2022-04-06 18:39:04 +0300

    basic read config

--- cmd/calculator.go
+++ cmd/calculator.go
@@ -1,7 +1,18 @@
 package main
 
-func main()  {
+import (
+	"fmt"
+	"go.uber.org/zap"
+	"os"
+	//"github.com/HeOpuHaMeH9I/CirquitCalculator/internal/shutdown/shutdown.go"
+)
+
+func main() {
+	logger, err := zap.NewProduction()
+	if err != nil {
+		fmt.Println("can't initialize logger")
+		os.Exit(1)
+	}
+	logger.Info("Starting CircuitCalculator")
 
 }
-
-.
---
+++ internal/shutdown/shutdown.go
@@ -0,0 +1,25 @@
+package shutdown
+
+import (
+	"go.uber.org/zap"
+	"io"
+	"os"
+	"os/signal"
+)
+
+func Graceful(logger *zap.Logger, signals []os.Signal, closeItems ...io.Closer) {
+	sigc := make(chan os.Signal, 1)
+	signal.Notify(sigc, signals...)
+	sig := <-sigc
+	logger.Warn("Caought termination signal. Shutting Down.",
+		zap.String("signal", sig.String()),
+	)
+
+	for _, closer := range closeItems {
+		if err := closer.Close(); err != nil {
+			logger.Error("failed to close",
+				zap.Any("closer", closer),
+				zap.Error(err))
+		}
+	}
+}
